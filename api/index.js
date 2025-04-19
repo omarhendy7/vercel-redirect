ce6e1f658a91de403931b3786b62e02f148775da
Omar Hendy <omarhendy@Omars-MacBook-Pro.local>
2025-04-19 13:55:29 +0200

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  const pathParts = req.url.split('/');
+  const type = pathParts[1]; // e.g. "job"
+  const id = pathParts[2];   // e.g. "123"
+
+  if (type === 'job' && id) {
+    res.writeHead(301, {
+      Location: `https://new.recruitera.ai/legacy-redirect?id=${id}`,
+    });
+    res.end();
+  } else {
+    res.writeHead(302, {
+      Location: `https://new.recruitera.ai`,
+    });
+    res.end();
+  }
+}
